9c6222fc37e78663380b0dcbec3e8f5b00657569
Peter Mangoro <petertmangoro@gmail.com>
2025-02-27 06:12:32 -0500

    Basic Crud on Hero

--- About_me/aboutme.sql
+++ About_me/aboutme.sql
@@ -1,2 +1,19 @@
--- Active: 1740649608635@@127.0.0.1@5432@aboutme@public
-SELECT * from place
+-- create table hero
+create table practice.hero (
+    id int,
+    name VARCHAR(255),
+    gender_code varchar(255)
+)
+
+-- insert
+insert into practice.hero (id,name,gender_code) VALUES
+(1,'John Doe','M'),
+(2,'Jane Dao','F')
+
+-- get results
+SELECT * FROM practice.hero
+
+-- update
+UPDATE practice.hero set name = 'Updated Doe' where id = 1
+
+DELETE from practice.hero where id = 1
